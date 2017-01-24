3c4906eb6d1118d0e70cd29c874f3024c5c439d6
Christian <Christian@CHRISTIAN-HP>
2017-01-24 12:29:11 +0100

    Mainwindow

--- PrettyHair.UI/MainWindow.xaml.cs
+++ PrettyHair.UI/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using PrettyHair.UI.View;
 
 namespace PrettyHair.UI
 {
@@ -19,10 +20,30 @@ namespace PrettyHair.UI
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
+
+
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void buttonOrders_Click(object sender, RoutedEventArgs e)
+        {
+            Orders orders = new Orders();
+            orders.Show();
+        }
+
+        private void buttonCustomers_Click(object sender, RoutedEventArgs e)
+        {
+            Customer costumer = new Customer();
+            costumer.Show();
+        }
+
+        private void buttonItems_Click(object sender, RoutedEventArgs e)
+        {
+            Items items = new Items();
+            items.Show();
         }
     }
 }
