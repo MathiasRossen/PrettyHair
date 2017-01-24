3956e1bc25cf094364d73580e04ae310efd144fb
Mathias <Mathias@LAPTOP-84H7VQ2I>
2017-01-24 08:44:02 +0100

    Repo into BL

---
+++ PrettyHair.Core/Repositories/CustomerRepository.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PrettyHair.Core.Entities;
+using PrettyHair.Core.Interfaces;
+
+namespace PrettyHair.DAL.Repositories
+{
+    public class CustomerRepository
+    {
+        private Dictionary<int, ICustomer> Customers = new Dictionary<int, ICustomer>();
+        private int ID;
+
+        public Dictionary<int, ICustomer> GetAllCustomers()
+        {
+            return Customers;
+        }
+
+        public void CreateCustomer(ICustomer customer)
+        {
+            // Database call
+
+            AddCustomer(customer, NextID());
+        }
+
+        public void RemoveCustomerByID(int ID)
+        {
+            Customers.Remove(ID);
+        }
+
+        public void Clear()
+        {
+            Customers.Clear();
+        }
+
+        public bool CustomerExistFromID(int ID)
+        {
+            return Customers.ContainsKey(ID);
+        }
+
+        public ICustomer GetCustomerByID(int ID)
+        {
+            return Customers[ID];
+        }
+
+        private int NextID()
+        {
+            return ++ID;
+        }
+
+        private void AddCustomer(ICustomer customer, int ID)
+        {
+            Customers.Add(ID, customer);
+        }
+    }
+}
---
+++ PrettyHair.Core/Repositories/ItemRepository.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+using PrettyHair.Core.Interfaces;
+using PrettyHair.Core.Entities;
+
+namespace PrettyHair.DAL.Repositories
+{
+    public class ItemRepository
+    {
+        private static string connectionstring = "DATABASE CONNECTION STRING";
+        private Dictionary<int, IItem> Items = new Dictionary<int, IItem>();
+        private int ID = 0;
+
+        public ItemRepository()
+        {
+
+        }
+
+        public Dictionary<int, IItem> GetItems()
+        {
+            return Items;
+        }
+
+        //public void RefreshItems()
+        //{
+        //    Clear();
+
+        //    using (SqlConnection db = new SqlConnection(connectionstring))
+        //    {
+        //        db.Open();
+
+        //        SqlCommand sql = new SqlCommand("SELECT * FROM ITEMS", db);
+        //        sql.CommandType = CommandType.Text;
+
+        //        SqlDataReader reader = sql.ExecuteReader();
+
+        //        if (reader.HasRows)
+        //        {
+        //            while (reader.Read())
+        //            {
+        //                IItem item       = new Item();
+        //                item.Name        = reader["ItemName"].ToString();
+        //                item.Description = reader["ItemDesc"].ToString();
+        //                item.Price       = (double)reader["ItemPrice"];
+        //                item.Amount      = (int)reader["ItemAmount"];
+
+        //                AddItem(item, (int)reader["ItemID"]);
+        //            }
+        //        }
+        //    }
+        //}
+
+        public void AddItems(string name, string description, double itemPrice, int quantity, int itemId)
+        {
+            //Clear();
+                      
+            IItem item = new Item();
+            item.Name = name;
+            item.Description = description;
+            item.Price = itemPrice;
+            item.Amount = quantity;
+                  
+            AddItem(item, itemId);
+        
+        }
+
+        public void CreateItem(IItem item)
+        {
+            using (SqlConnection db = new SqlConnection(connectionstring))
+            {
+                db.Open();
+
+                SqlCommand sql = new SqlCommand("InsertItem", db);
+                sql.CommandType = CommandType.StoredProcedure;
+
+                sql.Parameters.Add(new SqlParameter("ItemName",   item.Name));
+                sql.Parameters.Add(new SqlParameter("ItemDesc",   item.Description));
+                sql.Parameters.Add(new SqlParameter("ItemPrice",  item.Price));
+                sql.Parameters.Add(new SqlParameter("ItemAmount", item.Amount));
+
+                sql.ExecuteNonQuery();
+            }
+
+            AddItem(item, GetLastInsertedID());
+        }
+
+        public int GetLastInsertedID()
+        {
+            int ID = 0;
+
+            using (SqlConnection db = new SqlConnection(connectionstring))
+            {
+                db.Open();
+
+                SqlCommand sql  = new SqlCommand("LastItemID", db);
+                sql.CommandType = CommandType.StoredProcedure;
+                SqlDataReader reader = sql.ExecuteReader();
+
+                reader.Read();
+                ID = (int)reader["ItemID"];
+            }
+
+            return ID;
+        }
+
+        private void AddItem(IItem item, int ID)
+        {
+            Items.Add(ID, item);
+        }
+
+        public void RemoveItemByID(int ID)
+        {
+            using (SqlConnection db = new SqlConnection(connectionstring))
+            {
+                db.Open();
+
+                SqlCommand sql = new SqlCommand("RemoveItem", db);
+                sql.CommandType = CommandType.StoredProcedure;
+
+                sql.Parameters.Add(new SqlParameter("ID", ID));
+
+                sql.ExecuteNonQuery();
+            }
+
+            Items.Remove(ID);
+        }
+
+        public IItem GetItemByID(int ID)
+        {
+            return Items[ID];
+        }
+
+        public void Clear()
+        {
+            Items.Clear();
+        }
+
+        public int NextID()
+        {
+            return ++ID;
+        }
+    }
+}
