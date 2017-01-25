d706b66722a04df6cc20645da368383d0cd27833
Mathias <Mathias@LAPTOP-84H7VQ2I>
2017-01-25 15:27:17 +0100

    Small update to customer

--- PrettyHair.Core/Entities/Customer.cs
+++ PrettyHair.Core/Entities/Customer.cs
@@ -11,6 +11,13 @@ namespace PrettyHair.Core.Entities
     {
         public string Firstname { get; set; }
         public string Lastname { get; set; }
+        public string FullName
+        {
+            get
+            {
+                return Firstname + " " + Lastname;
+            }
+        }
         public long CustomerId { get; private set; }
 
         public Customer(string f, string l, long customerId)
